ef00307538a6a2bd9f8cdc26a7c5c8182927f8b1
mikan-juice <61767467+mikan-juice@users.noreply.github.com>
2020-09-22 16:42:14 +0900

    Add Autmatic update function

---
+++ app/assets/javascripts/autoReload.js
@@ -0,0 +1,66 @@
+$(function() {
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Chat-main__messages__comment" data-message-id=${message.id}>
+          <div class="Chat-main__messages__comment__info">
+            <div class="Chat-main__messages__comment__info__name">
+              ${message.user_name}
+            </div>
+            <div class="Chat-main__messages__comment__info__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Chat-main__messages__comment__text">
+            <p class="Chat-main__messages__comment__text__content">
+              ${message.content}
+            </p>
+          </div>
+            <img src="${message.image}">
+        </div>`
+      return html;
+    } else {
+      let html =
+        `<div class="Chat-main__messages__comment" data-message-id=${message.id}>
+          <div class="Chat-main__messages__comment__info">
+            <div class="Chat-main__messages__comment__info__name">
+              ${message.user_name}
+            </div>
+            <div class="Chat-main__messages__comment__info__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Chat-main__messages__comment__text">
+            <p class="Chat-main__messages__comment__text__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+
+  let reloadMessages = function() {
+    let last_message_id = $('.Chat-main__messages__comment:last').data("message-id") || 0;
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.Chat-main__messages').append(insertHTML);
+        $('.Chat-main__messages').animate({ scrollTop: $('.Chat-main__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  setInterval(reloadMessages, 7000);
+})
--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function() {
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="Chat-main__messages__comment">
+        `<div class="Chat-main__messages__comment" data-message-id=${message.id}>
           <div class="Chat-main__messages__comment__info">
             <div class="Chat-main__messages__comment__info__name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function() {
       return html;
     } else {
       let html =
-        `<div class="Chat-main__messages__comment">
+        `<div class="Chat-main__messages__comment" data-message-id=${message.id}>
           <div class="Chat-main__messages__comment__info">
             <div class="Chat-main__messages__comment__info__name">
               ${message.user_name}
