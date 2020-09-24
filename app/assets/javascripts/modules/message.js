caae4ed85cfc6542ad25c4227c0dad9657595dcf
mikan-juice <61767467+mikan-juice@users.noreply.github.com>
2020-09-24 16:34:46 +0900

    image class

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -16,7 +16,7 @@ $(function() {
               ${message.content}
             </p>
           </div>
-            <img src="${message.image}">
+            <img class="Chat-main__messages__comment__text__image" src="${message.image}">
         </div>`
       return html;
     } else {
